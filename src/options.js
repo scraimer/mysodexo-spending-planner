ae0d18c4bd78549f3cb7dad871e7f904736eb5f3
Shalom Craimer <Shalom.Craimer@LntTechServices.com>
2018-09-12 17:59:18 +0300

    Add multi-date picket in options page

--- src/options.js
+++ src/options.js
@@ -1,10 +1,8 @@
 // Saves options to chrome.storage
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
+  var vacationDays = $('#vacation-days').multiDatesPicker('value');
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
+    vacations: vacationDays
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -15,18 +13,62 @@ function save_options() {
   });
 }
 
+var my_options = {};
+
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-function restore_options() {
+function restore_options(callback) {
+	// Defaults:
+	let vacationDays = [
+		new Date( Date.UTC( 2018,  9 - 1, 18, 0,0,0) ),
+		new Date( Date.UTC( 2018,  9 - 1, 19, 0,0,0) ),
+		new Date( Date.UTC( 2018,  9 - 1, 23, 0,0,0) ),
+		new Date( Date.UTC( 2018,  9 - 1, 24, 0,0,0) ),
+		new Date( Date.UTC( 2018,  9 - 1, 30, 0,0,0) ),
+		new Date( Date.UTC( 2018, 10 - 1,  1, 0,0,0) ),
+	];
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
+     vacations: vacationDays
   }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+	  my_options = Object.assign({}, items);
+  	  callback();
   });
+
 }
-document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
+
+function after_options() {
+	/*
+	let vacationDays = [
+		new Date( Date.UTC( 2018,  9 - 1, 18, 0,0,0) ),
+		new Date( Date.UTC( 2018,  9 - 1, 19, 0,0,0) ),
+		new Date( Date.UTC( 2018,  9 - 1, 23, 0,0,0) ),
+		new Date( Date.UTC( 2018,  9 - 1, 24, 0,0,0) ),
+		new Date( Date.UTC( 2018,  9 - 1, 30, 0,0,0) ),
+		new Date( Date.UTC( 2018, 10 - 1,  1, 0,0,0) ),
+	];
+*/
+	let mdp = $('#vacation-days'); 
+	mdp.multiDatesPicker({
+//		addDates: vacationDays,
+		addDates: JSON.parse("[" + my_options.vacations + "]"),
+		numberOfMonths: 2,
+		dateFormat: '@', // Output unix timestamps (ms since 1970)
+		onSelect: function() { console.log( $('#vacation-days').multiDatesPicker('value') ); }
+	});
+	
+	/*
+	  let mdp = $('#vacation-days');
+	  mdp.multiDatesPicker('resetDates');
+	  mdp.multiDatesPicker('value', items.vacations);
+	  mdp.multiDatesPicker('toggleDate', 0 );
+	  mdp.multiDatesPicker('toggleDate', 0 );
+	*/
+}
+
+$( function() {
+	restore_options(after_options);
+});
+
