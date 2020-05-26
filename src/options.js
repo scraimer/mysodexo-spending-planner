7560c49c460bbea7a59216fb9267eef735733e67
Shalom Craimer <shalom.craimer@LTTS.com>
2020-05-26 08:20:55 +0300

    Fix error in options page preventing vacations calendars from being visible

--- src/options.js
+++ src/options.js
@@ -58,7 +58,7 @@ function after_options() {
 		dateFormat: '@', // Output unix timestamps (ms since 1970)
 		onSelect: function() { console.log( $('#vacation-days').multiDatesPicker('value') ); }
 	};
-	if( my_options && my_options.vacations.length > 0 )
+	if( my_options && my_options.vacations && my_options.vacations.length > 0 )
 	{
 		// We must not use 'addDates' if the array is empty. It prevents the entire UI from working
 		params.addDates = JSON.parse("[" + my_options.vacations + "]");
